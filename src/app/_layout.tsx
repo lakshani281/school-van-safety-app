355d26a62002b499aee6f7e41c33278e9cffb855
Lakshani Kaveesha <lakshanikaveesha992@gmail.com>
2026-09-24 18:45:24 +0530

    chore(router): register parent-fees route in root stack layout

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -20,6 +20,7 @@ export default function Layout() {
       <Stack.Screen name="parent-profile-view" />
       <Stack.Screen name="add-child" />
       <Stack.Screen name="parent-alerts" />
+      <Stack.Screen name="parent-fees" />
     </Stack>
   );
 }
