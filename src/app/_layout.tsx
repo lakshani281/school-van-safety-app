4d4ed3b9ff2e2ed8f269fe1467bf96d4702710c9
Lakshani Kaveesha <lakshanikaveesha992@gmail.com>
2026-09-24 18:21:09 +0530

    chore(router): register add-child modal route in root stack layout

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -18,6 +18,7 @@ export default function Layout() {
       <Stack.Screen name="parent-dashboard" />
       <Stack.Screen name="driver-profile-view" />
       <Stack.Screen name="parent-profile-view" />
+      <Stack.Screen name="add-child" />
     </Stack>
   );
 }
